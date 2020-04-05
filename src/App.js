773cba5d39f8ec122affa69523979ba6fce81d9d
Deepak <dd43028@gmail.com>
2020-04-05 18:25:00 +0530

    debounce

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import {debounce} from './debounce';
 
 function App(){
+    const handleClick = ()=>{
+        console.log("yoyooooo");
+    }
     return(
         <div>
            <h1>Hello World!</h1>
+           <button onClick={debounce(handleClick, 1000)}>Debounce</button>
         </div>
     )
 }
---
+++ src/debounce.js
@@ -0,0 +1,12 @@
+export const debounce = (fn, delay)=>{
+   let timerId;
+   return (...args)=>{
+       if(timerId){
+           clearTimeout(timerId);
+       }
+
+       timerId = setTimeout(()=>{
+          fn(...args);
+       }, delay)
+   }
+};
